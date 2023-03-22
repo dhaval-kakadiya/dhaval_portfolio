1c9def62e058a2d8bb18d2059164caec6df4eacd
NidhiItcode <nidhi.makdani.itcodehelp@gmail.com>
2023-03-22 21:38:58 +0530

    db link add

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
+mongoose.connect("mongodb+srv://dhaval:dhavalnode@cluster0.lpariun.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log(`Database Successfully Connected`)
 }).catch(() => {
     console.log(`Database connection Failed`)
