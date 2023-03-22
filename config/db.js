bd4dd00d9d100fe45cfb7247d56f14c18ce62df4
NidhiItcode <nidhi.makdani.itcodehelp@gmail.com>
2023-03-22 21:52:07 +0530

    edit vercel file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://dhaval:dhavalnode@cluster0.lpariun.mongodb.net/?retryWrites=true&w=majority").then(() => {
-    console.log(`Database Successfully Connected`)
-}).catch(() => {
-    console.log(`Database connection Failed`)
-});
+mongoose
+  .connect(
+    "mongodb+srv://dhaval:dhavalnode@cluster0.lpariun.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log(`Database Successfully Connected`);
+  })
+  .catch(() => {
+    console.log(`Database connection Failed`);
+  });
